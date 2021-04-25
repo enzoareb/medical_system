29e1c5f2807a302c245cbb2e85990af85d0c3d26
enzoareb <69877027+enzoareb@users.noreply.github.com>
2021-04-24 22:28:26 -0300

    add main

---
+++ Main.java
@@ -0,0 +1,49 @@
+import model.Doctor;
+import model.Patient;
+import model.User;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+    /*
+        Doctor myDoctor = new Doctor("James Fraser","jfraser@liberty.com");
+        myDoctor.showDataUser();
+        myDoctor.addAvailableAppointment(new Date(),"09.00 am");
+        myDoctor.addAvailableAppointment(new Date(),"12.30 pm");
+        myDoctor.addAvailableAppointment(new Date(),"14.00 pm");
+        System.out.println(myDoctor);
+
+        System.out.println();
+        System.out.println();
+
+        Patient patient = new Patient("alejandra","alejandra@gamil.com");
+        patient.showDataUser();
+        patient.setWeight(56.7);
+        patient.setHeight(1.60);
+        patient.setPhoneNumber("24584086");
+        System.out.println(patient);
+
+         clase anonima
+        forma de instanciar una clase abstracta (hack)
+        para darle un comportamiento determinado a un metodo en un momento determinado
+        ej. en programacion movil se utiliza para darle un comportamiento de terminado al
+        metodo onClickListener()
+
+        User user = new User("anonimo","anonimo@anonimo.com") {
+            @Override
+            public void showDataUser() {
+                //comportamiento
+            }
+        };
+
+        */
+        ui.UImenu.showMenu();
+
+        /*static permite acceder a un atributo o metodo solo llamando a su clase
+        sin necesidad de crear un objeto(ej. clase math)
+        double valor = Math.PI;
+        System.out.println(valor);
+         */
+    }
+}
